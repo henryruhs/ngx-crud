694c65772908a2d43086615e3fb320d922c994c4
redaxmedia <info@redaxmedia.com>
2019-12-17 16:15:10 +0100

    Fix coding style

--- src/cache.interceptor.ts
+++ src/cache.interceptor.ts
@@ -16,33 +16,33 @@ import { CacheService } from './cache.service';
 @Injectable()
 export class CacheInterceptor implements HttpInterceptor
 {
-	constructor(protected cacheService: CacheService)
+	constructor(protected cacheService : CacheService)
 	{
 	}
 
-	public intercept<T>(request: HttpRequest<T>, next: HttpHandler): Observable<HttpEvent<T>>
+	public intercept<T>(request : HttpRequest<T>, next : HttpHandler) : Observable<HttpEvent<T>>
 	{
-		const doCache: boolean = request.headers.has(CacheEnum.cacheMethod) &&
+		const doCache : boolean = request.headers.has(CacheEnum.cacheMethod) &&
 			request.headers.get(CacheEnum.cacheMethod) === request.method &&
 			request.headers.has(CacheEnum.cacheExpiration);
 
 		return doCache ? this.getRequest(request, next) : next.handle(request);
 	}
 
-	protected getRequest<T>(request: HttpRequest<T>, next: HttpHandler): Observable<HttpEvent<T>>
+	protected getRequest<T>(request : HttpRequest<T>, next : HttpHandler) : Observable<HttpEvent<T>>
 	{
-		const cachedResponse: Observable<HttpEvent<T>> = this.cacheService.clearInvalid().get(request);
+		const cachedResponse : Observable<HttpEvent<T>> = this.cacheService.clearInvalid().get(request);
 
 		return cachedResponse ? cachedResponse : this.storeRequest(request, next);
 	}
 
-	protected storeRequest<T>(request: HttpRequest<T>, next: HttpHandler): Observable<HttpResponse<T>>
+	protected storeRequest<T>(request : HttpRequest<T>, next : HttpHandler) : Observable<HttpResponse<T>>
 	{
 		const nextHandler : Observable<HttpResponse<T>> = next
 			.handle(request)
 			.pipe(
 				filter(event => event instanceof HttpResponse),
-				tap((response: HttpResponse<T>) => this.cacheService.set(request, of(response))),
+				tap((response : HttpResponse<T>) => this.cacheService.set(request, of(response))),
 				publishReplay(),
 				refCount()
 			);
