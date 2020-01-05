fbebe4e047a52289b3bd0ca6c935fa7d9ac4a9a7
redaxmedia <info@redaxmedia.com>
2020-01-05 22:31:42 +0100

    Fix test:watch

--- tests/setup.js
+++ tests/setup.js
@@ -6,4 +6,6 @@ require('jsdom-global/register');
 const testing = require('@angular/core/testing');
 const browser = require('@angular/platform-browser-dynamic/testing');
 
+testing.TestBed.resetTestEnvironment();
 testing.TestBed.initTestEnvironment(browser.BrowserDynamicTestingModule, browser.platformBrowserDynamicTesting());
+
